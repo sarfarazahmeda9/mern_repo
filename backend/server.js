629ec37190446902ed7e3bc3f16ea608e1501504
sarfaraz.ahmed.a9@gmail.com <sarfaraz.ahmed.a9@gmail.com>
2024-10-02 20:46:20 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,29 @@ import path from 'path';
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse json data in the request body
 
 //path.join(__dirname, '../frontend/build');
+//app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.use("/api/products", productRoutes);
 
 app.use("/api/brands", brandRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+else{   
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.listen(PORT,()=>{
     connectDB();
