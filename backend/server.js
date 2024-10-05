5a84367fce90082e3f0d052384afc102785d5594
sarfaraz.ahmed.a9@gmail.com <sarfaraz.ahmed.a9@gmail.com>
2024-10-06 00:07:22 +0530

    push file server.s

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,7 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     });
 }
-else{   
-    app.get('/', (req, res) => {
-        res.send('API is running...');
-    });
-}
+
 
 app.listen(PORT,()=>{
     connectDB();
